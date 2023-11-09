819466d6fc5dd4c536559915d3f797de9b42e09b
Donato-Carola <don94@live.it>
2023-11-09 15:36:14 +0100

    modifica dell'esercizio usando i template literal nel file js

--- js/file.js
+++ js/file.js
@@ -5,7 +5,9 @@ const cognome = prompt('Inserisci il tuo cognome');
 const color = prompt('Inserisci il tuo colore preferito');
 const number = 21;
 
-const date = name + cognome + color + number;
+//const date = name + cognome + color + number;
+
+const date = (`${name}${cognome}${color}${number}`) ;
 
 document.getElementById('text').innerHTML=  ' I dati dell\'utente sono: ' ;
 document.getElementById('date_user').innerHTML=  date; 
