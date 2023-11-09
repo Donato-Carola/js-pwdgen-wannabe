b5d46694ed1f35600a3a4922d7f7ad6e821e3d96
Donato-Carola <don94@live.it>
2023-11-09 15:27:34 +0100

    modifiche in js

--- js/file.js
+++ js/file.js
@@ -3,8 +3,9 @@
 const name = prompt('Inserisci il tuo nome');
 const cognome = prompt('Inserisci il tuo cognome');
 const color = prompt('Inserisci il tuo colore preferito');
+const number = 21;
 
-const date = name + ' ' + cognome + ' ' + color;
+const date = name + cognome + color + number;
 
 document.getElementById('text').innerHTML=  ' I dati dell\'utente sono: ' ;
 document.getElementById('date_user').innerHTML=  date; 
